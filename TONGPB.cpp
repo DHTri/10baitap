e561a90769eae734463b7d5b9c118edd4dc225fd
DHTri <145561569+DHTri@users.noreply.github.com>
2024-09-05 15:25:34 +0700

    Create TONGPB.cpp

---
+++ TONGPB.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+int main()
+{
+    int t;cin>>t;
+    while(t--)
+    {
+        int n;cin>>n;
+        cout<<(n*(n+1)*(2*n+1))/6<<endl;
+    }
+} 
